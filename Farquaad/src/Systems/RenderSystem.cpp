0a436ca2c525294e16805c81e4a1103491b6050a
Stephen Ma <tehphen@gmail.com>
2015-12-22 13:21:01 -0800

    Try to fix gcc and clang builds.

--- Farquaad/src/Systems/RenderSystem.cpp
+++ Farquaad/src/Systems/RenderSystem.cpp
@@ -1,5 +1,5 @@
 // Copyright 2015 Bablawn3d5
-#include <Farquaad\Systems\RenderSystem.h>
+#include <Farquaad/Systems/RenderSystem.h>
 
 void RenderSystem::update(ex::EntityManager & em,
                           ex::EventManager & events, ex::TimeDelta dt) {
