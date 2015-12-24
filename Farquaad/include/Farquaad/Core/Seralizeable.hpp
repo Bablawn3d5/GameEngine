fce5c628878fe9916ba8e61509ea6a100dc218ee
Stephen Ma <tehphen@gmail.com>
2015-12-23 22:12:56 -0800

    Move SeralizeableHandle definition to the top.

--- Farquaad/include/Farquaad/Core/Seralizeable.hpp
+++ Farquaad/include/Farquaad/Core/Seralizeable.hpp
@@ -2,8 +2,19 @@
 
 #pragma once
 
-#include <string>
 #include <json/json.h>
+#include <string>
+
+template<typename T>
+class SeralizeableHandle {
+public:
+    static const std::string rootName;
+    SeralizeableHandle() {}
+    ~SeralizeableHandle<T>() {}
+
+    T fromJSON(const Json::Value&) const = 0;
+    Json::Value toJSON(const T& component) const = 0;
+};
 
 // Defines that a component can be serialized by a ComponentSerializer
 template<typename T>
@@ -15,26 +26,15 @@ public:
     static inline T fromJSON(const Json::Value& json) {
         SeralizeableHandle<T> handle;
         return handle.fromJSON(json);
-    };
+    }
 
     static inline Json::Value toJSON(const T& component) {
         SeralizeableHandle<T> handle;
         return handle.toJSON(component);
-    };
+    }
 
     static Json::Value getValueByRootName(const Json::Value& value) {
         std::string componentName = SeralizeableHandle<T>::rootName;
         return value[componentName];
     }
-};
-
-template<typename T>
-class SeralizeableHandle {
-public:
-    static const std::string rootName;
-    SeralizeableHandle() {}
-    ~SeralizeableHandle<T>() {}
-
-    T fromJSON(const Json::Value&) const = 0;
-    Json::Value toJSON(const T& component) const = 0;
 };
