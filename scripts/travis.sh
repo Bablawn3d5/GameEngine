a25c469e202422480d12355fb96f4d4e8c72295b
Stephen Ma <tehphen@gmail.com>
2015-12-23 23:35:54 -0800

    Disable entityx testing when building gameengine.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-cmake -DCMAKE_INSTALL_PREFIX="`pwd`" -DCMAKE_BUILD_TYPE=Debug
+cmake -DCMAKE_INSTALL_PREFIX="`pwd`" -DCMAKE_BUILD_TYPE=Debug -DENTITYX_BUILD_TESTING=0
 make VERBOSE=1
 make test
 cpack
